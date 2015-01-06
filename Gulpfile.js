f386b6dac7872e55f8ca36272c751b363a11147d
bound1ess <bound1ess.dev@gmail.com>
2015-01-07 01:23:42 +0600

    Add JS minification and watcher, update index.html.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require("gulp"),
     util = require("gulp-util"),
     minifyHtml = require("gulp-minify-html"),
     less = require("gulp-less"),
-    minifyCss = require("gulp-minify-css");
+    minifyCss = require("gulp-minify-css"),
+    minifyJs = require("gulp-uglify");
 
 gulp.task("html", function() {
     util.log("Minifying...");
@@ -25,11 +26,22 @@ gulp.task("less", function() {
     util.log("Done!");
 });
 
+gulp.task("js", function() {
+    util.log("Minifying...");
+
+    gulp.src("src/js/*.js")
+        .pipe(minifyJs({mangle: false}))
+        .pipe(gulp.dest("public/js/"));
+
+    util.log("Done!");
+});
+
 gulp.task("watch", function() {
     gulp.watch("src/html/*.html", ["html"]);
     gulp.watch("src/less/**/*.less", ["less"]);
+    gulp.watch("src/js/*.js", ["js"]);
 });
 
-gulp.task("default", ["html", "less", "watch"], function() {
+gulp.task("default", ["html", "less", "js", "watch"], function() {
     util.log("Done!");
 });
---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+(function() {
+    var todo = angular.module("todo", []);
+})();

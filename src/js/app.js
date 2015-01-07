dc36081cfec2aa9226e2e08b43cad3a809b67a77
bound1ess <bound1ess.dev@gmail.com>
2015-01-07 17:42:24 +0600

    Update index.html, add three columns for (un)completed tasks and submit form.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
 (function() {
     var todo = angular.module("todo", []);
+
+    todo.service("Task", ["$rootScope", function($rootScope) {
+        var service = {
+            tasks: [],
+            
+            addTask: function(task) {
+                service.tasks.push(task);
+                $rootScope.$broadcast("tasks.update");
+            }
+        };
+
+        return service;
+    }]);
 })();

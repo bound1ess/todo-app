bb0527b3a49c583685756751035832dab25324e4
bound1ess <bound1ess.dev@gmail.com>
2015-01-10 00:59:08 +0600

    Everything seems to work pretty well.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express"), app = express();
 var fs = require("fs"), bodyParser = require("body-parser");
 
+var tasksFile = __dirname + "/public/data/tasks.json";
+
 app.use("/public", express.static(__dirname + "/public"));
 
 app.use(bodyParser.json({extended: false}));
@@ -10,14 +12,16 @@ app.get("/", function(request, response) {
 });
 
 app.post("/tasks/add", function(request, response) {
-    var tasksFile = __dirname + "/public/data/tasks.json", tasks;
+    var tasks;
 
     fs.readFile(tasksFile, "utf8", function(error, data) {
-        if (error === null) {
-            tasks = JSON.parse(data).tasks;
+        if (error !== null) {
+            return false;
         }
 
+        tasks = JSON.parse(data).tasks;
         tasks.push(request.body);
+
         fs.writeFile(tasksFile, JSON.stringify({tasks: tasks}, null, 4), "utf8");
     });
 
@@ -25,7 +29,22 @@ app.post("/tasks/add", function(request, response) {
 });
 
 app.post("/tasks/update", function(request, response) {
-    console.log("OK, got the request.");
+    var taskId = parseInt(request.body.id) - 1, tasks;
+    
+    fs.readFile(tasksFile, "utf8", function(error, data) {
+        if (error !== null) {
+            return false;
+        }
+
+        tasks = JSON.parse(data).tasks;
+
+        if (tasks[taskId] !== undefined) {
+            tasks[taskId].isCompleted = !tasks[taskId].isCompleted;
+        }
+        
+        fs.writeFile(tasksFile, JSON.stringify({tasks: tasks}, null, 4), "utf8");
+    });
+
     response.send("OK");
 });
 

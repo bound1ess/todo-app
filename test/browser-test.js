62ebb8d9765c965870212b8a9d985fdbaee28fcb
bound1ess <bound1ess.dev@gmail.com>
2015-01-13 19:39:10 +0600

    Finish #addTask() section.

--- test/browser-test.js
+++ test/browser-test.js
@@ -11,6 +11,17 @@ describe("Task", function() {
     describe("#addTask()", function() {
         it("adds a task", function() {
             expect(Task.tasks).to.be.empty();
+
+            Task.addTask("Sample task");
+            
+            expect(Task.tasks).to.not.be.empty();
+            expect(Task.tasks).to.have.length(1);
+
+            expect(Task.tasks[0]).to.be.an("object");
+            
+            expect(Task.tasks[0].id).to.equal(1);
+            expect(Task.tasks[0].body).to.equal("Sample task");
+            expect(Task.tasks[0].isCompleted).to.be.false();
         }); 
     });
 

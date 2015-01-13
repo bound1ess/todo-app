5603c53f4157acc3b23a5ccabaa8f26d3dccf766
bound1ess <bound1ess.dev@gmail.com>
2015-01-13 21:37:00 +0600

    Test TasksController.

--- test/browser-test.js
+++ test/browser-test.js
@@ -1,12 +1,16 @@
-describe("Task", function() {
-    var Task;
+describe("ToDo application", function() {
+    var Task, controller, scope;
 
     beforeEach(module("todo"));
 
     // Leading and trailing underscores will be stripped away.
-    beforeEach(inject(function(_Task_, _$httpBackend_) {
+    beforeEach(inject(function(_Task_, _$httpBackend_, $controller, $rootScope) {
         Task = _Task_;
         $httpBackend = _$httpBackend_;
+
+        controller = $controller("TasksController", {
+            $scope: (scope = $rootScope.$new())
+        });
 
         $httpBackend
             .expectGET("/public/data/tasks.json")
@@ -15,7 +19,7 @@ describe("Task", function() {
         $httpBackend.flush();
     }));
 
-    describe("#addTask()", function() {
+    describe("Task service", function() {
         it("adds a task", function() {
             expect(Task.tasks).to.be.empty();
 
@@ -39,7 +43,47 @@ describe("Task", function() {
             expect(Task.tasks[0].id).to.equal(1);
             expect(Task.tasks[0].body).to.equal("Sample task");
             expect(Task.tasks[0].isCompleted).to.be.false();
-        }); 
+        });
+    });
+
+    describe("Tasks controller", function() {
+        it("sets proper scope values", function() {
+            expect(scope.tasks).to.be.an("array");
+            expect(scope.tasks).to.be.empty();
+
+            expect(scope.newTask).to.be.a("string");
+            expect(scope.newTask).to.be.empty();
+        });
+
+        it("adds a task (delegated to the Task service)", function() {
+            scope.newTask = "Sample task";
+
+            $httpBackend
+                .expectPOST("/tasks/add", {
+                    id: 1,
+                    body: "Sample task",
+                    isCompleted: false
+                })
+                .respond(200, "OK");
+
+            scope.addTask();
+
+            $httpBackend.flush();
+
+            expect(scope.newTask).to.be.empty();
+        });
+
+        it("updates a task's state (delegated to the Task service)", function() {
+            $httpBackend
+                .expectPOST("/tasks/update", {
+                    id: 1
+                })
+                .respond(200, "OK");
+        
+            scope.onTaskChange(1);
+
+            $httpBackend.flush();
+        });
     });
 
 });

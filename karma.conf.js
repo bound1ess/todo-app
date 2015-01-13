afab00a750385847b0edebc8ff1727671d33b2e9
bound1ess <bound1ess.dev@gmail.com>
2015-01-13 22:30:51 +0600

    Add karma-coverage.

--- karma.conf.js
+++ karma.conf.js
@@ -30,13 +30,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/js/app.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots'],
+    reporters: ['dots', 'coverage'],
 
 
     // web server port
@@ -63,6 +64,11 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
+    singleRun: true,
+
+    coverageReporter: {
+      type: 'text-summary',
+      dir: 'coverage/'
+    }
   });
 };
